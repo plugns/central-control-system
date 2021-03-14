50507304654781c806f82e090d83718994724ab4
João Paulo S Bertoncini <joao.bertoncini@gmail.com>
2021-03-14 14:37:41 -0300

    new features

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
-
 import socket
 import threading
 import os
 from dotenv import load_dotenv
-from Modules import calibrate_camera
+import cv2
+import imutils
+import math
+from Modules import module_calibrate_camera
 
 load_dotenv(verbose=True)
 
 HOST = os.getenv("HOST")
 PORT = int(os.getenv("PORT"))
+DEVICE_NUMBER = int(os.getenv("DEVICE_NUMBER"))
 
 
 class ClientThread(threading.Thread):
     def __init__(self, clientAddress, clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
-        print("New connection added: ", clientAddress)
-        print("Connection from : ", clientAddress)
+        self.caddress = clientAddress
+        print("New Robot connection added: ", clientAddress)
 
     def run(self):
         # self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
         msg = ''
+        cap = init_vision_system(DEVICE_NUMBER)
         while True:
+            _, frame = cap.read()
+            cv2.imshow("Cenario", frame)
             data = self.csocket.recv(2048)
             if data:
                 msg = data.decode()
@@ -30,7 +36,7 @@ class ClientThread(threading.Thread):
                     break
                 print("from client", msg)
                 self.csocket.send(bytes(msg, 'UTF-8'))
-        # print("Client at ", clientAddress, " disconnected...")
+        print("Client at ", self.caddress, " disconnected...")
 
 
 def init_system():
@@ -38,13 +44,32 @@ def init_system():
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((HOST, PORT))
     print("Server started")
-    print("Waiting for client request..")
+    print("Waiting for Robot request..")
     while True:
         server.listen(1)
         clientsock, clientAddress = server.accept()
         newthread = ClientThread(clientAddress, clientsock)
         newthread.start()
 
+
+def init_vision_system(device_number):
+    cap = cv2.VideoCapture(device_number)
+    if cap.isOpened():
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+        # print(cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT) # 3, 4
+        print('width, height:', width, height)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        print('fps:', fps)  # float
+        # print(cv2.CAP_PROP_FPS) # 5
+        frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        print('frames count:', frame_count)  # float
+        # print(cv2.CAP_PROP_FRAME_COUNT) # 7
+        return cap
+    else:
+        print('Error on open video device', device_number)
 
 def print_menuCentral():  ## Your menu design here
     print(24 * "-", "CENTRAL CONTROL SYSTEM", 24 * "-")
@@ -67,7 +92,7 @@ def main():
             init_system()
         if choice == 2:
             print(">> Starting Calibration Camera")
-            calibrate_camera.CalibrationCamera()
+            module_calibrate_camera.CalibrationCamera()
         elif choice == 3:
             print(">> Exit")
             quit()
